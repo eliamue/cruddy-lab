46e473eaa17a71ca2129a6454539e59081fefe09
Elise Muellerleile <eliamue@live.com>
2021-07-23 17:57:28 -0500

    vocalsit model created, missing part of test added, test passes

--- __tests__/vocalists.test.js
+++ __tests__/vocalists.test.js
@@ -25,32 +25,39 @@ describe('vocalists routes', () => {
   });
 
   it('gets all vocalists', async () => {
-    const strayKids = await Vocalists.createVocalist({
+    const gIdle = await Vocalists.createVocalist(
+      {
         kgroup: '(G)I-dle', 
         name: 'Minnie'
-    });
+      });
 
-    const mamamoo = await Vocalists.createVocalist({
+    const mamamoo = await Vocalists.createVocalist(
+      {
         kgroup: 'Mamamoo', 
         name: 'Hwasa'
-    });
+      });
 
     const bts = await Vocalists.createVocalist(
-        {
+      {
         kgroup: 'BTS', 
         name: 'V'
-        });
+      });
 
     const shinee = await Vocalists.createVocalist(
-        {
+      {
         kgroup: 'SHINee', 
         name: 'Taemin'
-        });
+      });
 
-    const ateez = await Vocalists.createVocalist(
-        {
+    const itzy = await Vocalists.createVocalist(
+      {
         kgroup: 'Itzy', 
         name: 'Lia'
-        });
+      });
+        
+    const res = await request(app)
+      .get('/api/v1/vocalists');
 
+    expect(res.body).toEqual([gIdle, mamamoo, bts, shinee, itzy]);
+  });
 });
