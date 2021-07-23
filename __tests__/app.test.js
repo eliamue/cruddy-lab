e2168a6b71e1737d282b871f360190de31896b85
Elise Muellerleile <eliamue@live.com>
2021-07-23 14:40:37 -0500

    get all rappers test created

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ import pool from '../lib/utils/pool';
 import setup from '../data/setup';
 import request from 'supertest';
 import app from '../lib/app';
+import Rappers from '../lib/models/Rappers';
 
 describe('rappers routes', () => {
   beforeEach(() => {
@@ -18,5 +19,33 @@ describe('rappers routes', () => {
       id: '1',
       ...rapper
     });
+  });
+  
+  it('gets all rappers', async () => {
+    const allRappers = await Rappers.insert(
+      [
+        {
+          kgroup: 'Stray Kids', 
+          name: 'Hyunjin'
+        },
+        {
+          kgroup: 'Mamamoo', 
+          name: 'Moonbyul'
+        },
+        {
+          kgroup: 'BTS', 
+          name: 'Suga'
+        },
+        {
+          kgroup: 'SHINee', 
+          name: 'Minho'
+        },
+      ]
+    );
+    const res = await request(app)
+      .post('/api/vy/rappers')
+      .send(allRappers);
+
+    expect(res.body).toEqual([allRappers]);
   });
 });
