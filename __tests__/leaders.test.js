c0e064bff9a0a5071a3ce18190f1936a6738bcda
Elise Muellerleile <eliamue@live.com>
2021-07-24 22:49:24 -0500

    leaders table added to sql and appjs, test page created and initialized, create leader test written

---
+++ __tests__/leaders.test.js
@@ -0,0 +1,28 @@
+import pool from '../lib/utils/pool';
+import setup from '../data/setup';
+import request from 'supertest';
+import app from '../lib/app';
+import Leaders from '../lib/models/Leaders';
+
+describe('group leaders CRUD routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('creates a new group leader', async () => {
+    const mamamoo = {
+      kgroup: 'Mamamoo',
+      stage_name: 'Solar',
+      real_name: 'Kim Yong-sun'
+    };
+    const res = await request(app)
+      .post('/api/v1/leaders')
+      .send(mamamoo);
+
+    expect(res.body).toEqual({
+      id: '1',
+      ...mamamoo
+    });
+  });
+
+});
