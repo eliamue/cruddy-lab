eafdede5a4b1ff872d78153bdf8a63d5bff671e0
Elise Muellerleile <eliamue@live.com>
2021-07-26 12:36:20 -0500

    qweqwd

--- visuals-test.js
+++ visuals-test.js
@@ -1,8 +1,8 @@
-import pool from '../lib/utils/pool.js';
-import setup from '../data/setup.js';
+import pool from './lib/utils/pool.js';
+import setup from './data/setup.js';
 import request from 'supertest';
-import app from '../lib/app.js';
-import Visuals from '../lib/models/Visuals.js';
+import app from './lib/app.js';
+import Visuals from './lib/models/Visuals.js';
 
 describe('visuals routes', () => {
   beforeEach(() => {
